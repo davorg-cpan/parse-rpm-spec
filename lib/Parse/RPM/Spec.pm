dd6cfc1536c4772986dd1608c7afb7eed78ffb2b
Dave Cross <dave@dave.org.uk>
2023-01-13 10:50:48 +0000

    Prep for release

--- lib/Parse/RPM/Spec.pm
+++ lib/Parse/RPM/Spec.pm
@@ -7,7 +7,7 @@ use warnings;
 use Carp;
 use Moose;
 
-our $VERSION = 'v1.1.1';
+our $VERSION = 'v1.1.2';
 
 has file          => ( is => 'ro', isa => 'Str', required => 1 );
 has name          => ( is => 'rw', isa => 'Str' );
